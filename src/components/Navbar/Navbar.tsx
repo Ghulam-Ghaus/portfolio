7368d0add179364bbaa307150c623c10de6a3bb2
ghulam.ghaus <ghulam.ghaus@hive-worx.com>
2024-06-30 14:22:42 +0300

    menu update

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,26 +4,23 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="fixed  gap-6 top-0 right-4 h-full flex flex-col justify-center items-end">
-      {menuItems.map((menuItem, index) => {
-        return (
-          <Link
-            key={index}
-            href={menuItem.href}
-            passHref
-            className="group relative flex flex-col items-center justify-center"
-          >
-            <div className="flex flex-row-reverse items-center transition-width duration-300 ease-in-out transition-color  overflow-hidden whitespace-nowrap rounded-full group-hover:bg-[#FFB400]">
-              <div className="p-3.5 rounded-full bg-gray-300 group-hover:bg-[#FFB400]">
-                {menuItem.icon}
-              </div>
-              <span className="label font-medium group-hover:translate-x-0 group-hover:text-white">
-                {menuItem.label}
-              </span>
+    <nav className="fixed gap-6 top-0 right-6 h-full flex flex-col justify-center items-end">
+      {menuItems.map(({ href, icon, label }, index) => (
+        <Link
+          key={index}
+          href={href}
+          className="group relative flex flex-col items-center justify-center"
+        >
+          <div className="flex flex-row-reverse items-center transition-width duration-300 ease-in-out overflow-hidden whitespace-nowrap rounded-full group-hover:bg-[#FFB400]">
+            <div className="p-3.5 rounded-full bg-gray-300 group-hover:bg-[#FFB400]">
+              {icon}
             </div>
-          </Link>
-        );
-      })}
+            <span className="label font-medium group-hover:translate-x-0 group-hover:text-white">
+              {label}
+            </span>
+          </div>
+        </Link>
+      ))}
     </nav>
   );
 };
